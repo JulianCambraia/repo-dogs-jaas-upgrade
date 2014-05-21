e0d97423519197769c414bccc75ba13f09e17de8
JulianFernando <juliancambraia@gmail.com>
2014-05-20 22:36:57 -0300

    - Permite usar as Tags do JSF e Prime Faces em conjunto com j_security_check - Volta a forma de validação de Login. - Remove Managed Bean.

--- crudJSF/src/br/com/jfac/managedBeans/UserMB.java
+++ crudJSF/src/br/com/jfac/managedBeans/UserMB.java
@@ -1,16 +1,11 @@
 package br.com.jfac.managedBeans;
 
-import java.security.Principal;
-
 import javax.ejb.EJB;
 import javax.enterprise.context.SessionScoped;
-import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
 
 import br.com.jfac.facade.UserFacade;
 import br.com.jfac.model.User;
@@ -24,16 +19,15 @@ public class UserMB {
 	@EJB
 	private UserFacade userFacade;
 
-	HttpServletRequest request = (HttpServletRequest) FacesContext.getCurrentInstance().getExternalContext().getRequest();
-	HttpSession session = (HttpSession) FacesContext.getCurrentInstance().getExternalContext().getSession(false);
-	
 	public User getUser() {
 		if (user == null) {
+			
 			ExternalContext context = FacesContext.getCurrentInstance().getExternalContext();
 			
 			String userEmail = context.getUserPrincipal().getName();
 			user = userFacade.findUserByEmail(userEmail);
 		}
+		
 		return user;
 	}
 
@@ -51,40 +45,4 @@ public class UserMB {
 		return (HttpServletRequest) FacesContext.getCurrentInstance().getExternalContext().getRequest();
 	}
 	
-	public String login() {
-		String message = "";
-		String navigator = "";
-
-		try {
-
-			request.login(user.getName(), user.getPassword());
-			Principal principal = request.getUserPrincipal();
-
-			if (request.isUserInRole("Administrator")) {
-				message = "Usuário : "
-						+ principal.getName()
-						+ " Você é Administrador e tem direito a todos as funcionalidades!";
-				navigator = "admin";
-			} else if (request.isUserInRole("Manager")) {
-				message = "Usuário : "
-						+ principal.getName()
-						+ "Você é um Diretor e pode visualizar todos os relatórios gerenciais!";
-				navigator = "manager";
-			} else if (request.isUserInRole("Operator")) {
-				message = "Usuário : " + principal.getName()
-						+ "Você é um Operador, Por Favor Atenda bem o cliente!";
-				navigator = "operator";
-			}
-
-			FacesContext.getCurrentInstance().addMessage(null,new FacesMessage(FacesMessage.SEVERITY_INFO,message, null));
-			
-			return navigator;
-			
-		} catch (ServletException e) {
-			FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Ocorreu algum problema e o Login Falhou!", null));
-			e.printStackTrace();
-		}
-		
-		return "failure";
-	}
 }
